f439d06b7e7a96b69da9e0bc7184671b2cadc3db
Felipe Koga <felipe.koga@hotmail.com>
2020-08-16 00:19:51 -0300

    refactor:  remove comment lines from routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,9 @@
 /* eslint-disable new-cap */
 import { Router } from 'express';
 import UserController from '@controllers/UserController';
-// import AuthController from "./controllers/AuthController";
-// import authMiddleware from "./middlewares/auth";
 
 const routes = Router();
-// routes.use(authMiddleware);
 routes.get('/users', UserController.list);
 routes.post('/users', UserController.create);
-// routes.post("/register", UserController.create);
-// routes.get("/authenticate", AuthController.authenticate);
 
 export default routes;
